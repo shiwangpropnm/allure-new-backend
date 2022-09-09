9176840d0c908344e212f970fdbbf86949b17b03
shiwangpropnm <110289926+shiwangpropnm@users.noreply.github.com>
2022-09-09 16:13:49 +0530

    Add files via upload

---
+++ allure/src/test/java/com/allure/AllureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
